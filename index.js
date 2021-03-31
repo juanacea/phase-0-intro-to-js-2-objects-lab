f24f26569efb892e44d533eaeaea7613dd77f3da
juanacea <juan.acea93@gmail.com>
2021-03-30 23:04:45 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {
+    Juan: '1 Woodbridge Center'
+}
+function updateEmployeeWithKeyAndValue(employee, name, address){
+    const newEmployee = {...employee};
+    newEmployee[name] = address;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, address){
+    employee[name] = address;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name;
+    return employee;
+}
